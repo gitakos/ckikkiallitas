2f7c3367a388a6526676f6c180841986f22d67d5
bUMmrR <agocsdaniel5454@gmail.com>
2023-02-18 12:52:00 +0100

    A daro Ascotta oldal teljesen készen van, a tldr kapott egy átmenetet, ami még erősen buggos

--- tldr.js
+++ tldr.js
@@ -1,14 +1,55 @@
-function toggleFullText(tldrId, fullTextId) {
+function toggleFullText(tldrId, fullTextId, buttonId) {
     var tldr = document.getElementById(tldrId);
     var fullText = document.getElementById(fullTextId);
-
+    var button = document.getElementById(buttonId);
+  
     if (tldr.style.display === "none") {
-      tldr.style.display = "block";
-      fullText.style.display = "none";
+      fullText.style.transition = "none";
+      tldr.style.transition = "none";
+
+
+      tldr.style.opacity = "0";
+      tldr.style.height = "0";
+      tldr.style.overflow = "hidden";
+      tldr.style.transition = "opacity 0.5s ease, height 0.5s ease";
+      fullText.style.opacity = "0";
+      fullText.style.height = "0";
+      fullText.style.overflow = "hidden";
+      fullText.style.transition = "opacity 0.5s ease, height 0.5s ease";
+      button.innerHTML = "Hosszabban";
+      setTimeout(function() {
+        tldr.style.display = "block";
+        fullText.style.display = "none";
+        setTimeout(function() {
+          tldr.style.opacity = "1";
+          tldr.style.height = tldr.scrollHeight + "px";
+          tldr.style.overflow = "visible";
+        }, 0);
+      }, 500);
     } else {
-      tldr.style.display = "none";
-      fullText.style.display = "block";
+        fullText.style.transition = "none";
+        tldr.style.transition = "none";
+
+
+
+      fullText.style.opacity = "0";
+      fullText.style.height = "0";
+      fullText.style.overflow = "hidden";
+      fullText.style.transition = "opacity 0.5s ease, height 0.5s ease";
+      tldr.style.opacity = "0";
+      tldr.style.height = "0";
+      tldr.style.overflow = "hidden";
+      tldr.style.transition = "opacity 0.5s ease, height 0.5s ease";
+      button.innerHTML = "Rövidebben";
+      setTimeout(function() {
+        fullText.style.display = "block";
+        tldr.style.display = "none";
+        setTimeout(function() {
+          fullText.style.opacity = "1";
+          fullText.style.height = fullText.scrollHeight + "px";
+          fullText.style.overflow = "visible";
+        }, 0);
+      }, 500);
     }
-}
-
-
+  }
+  
