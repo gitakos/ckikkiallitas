a6a32efc7b85484bea255a449a17ad46773bb6a7
Kovács Ákos <kovacsakos.005@gmail.com>
2023-04-12 18:21:21 +0200

    Kezdetleges automata html iro

---
+++ htmlIro/txthtmlbeKIALLITAS/txthtmlbeKIALLITAS/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace txthtmlbeKIALLITAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text Fálj kiválasztás";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kiválasztott fálj:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kiválaszt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generálás";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 118);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lustaság 1.0.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ htmlIro/txthtmlbeKIALLITAS/txthtmlbeKIALLITAS/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txthtmlbeKIALLITAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static bool faljKivalasztva = false;
+        static string faljNev;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string faljnev = textBox1.Text;
+                StreamReader r = new StreamReader(faljnev);
+                r.ReadLine();
+                r.Close();
+                faljKivalasztva = true;
+                faljNev = faljnev;
+                label2.Text = "Kiválasztott fálj: "+faljnev;
+            }
+            catch
+            {
+                label2.Text = "Kiválasztott fálj: Nincs ilyen fálj!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(faljKivalasztva)
+            {
+                gen();
+                label3.Text = "Generálás sikeres!";
+            }
+            else
+            {
+                label3.Text = "Generálás sikertelen!";
+            }
+        }
+        static string template = "";
+        private void gen()
+        {
+            /*Formátum:
+            Cím
+            
+            Bek1Cím
+            Bek1Tartalom
+
+            Bek2Cím
+            Bek2Tartalom
+
+            Bek3Cím
+            Bek3Tartalom
+
+            Tábla:
+            CÍM|TARTALOM
+            CÍM|TARTALOM
+            CÍM|TARTALOM
+            CÍM|TARTALOM
+            */
+            StreamReader r = new StreamReader(faljNev);
+            bool bekezdesbenvagye = false;
+            string cim = r.ReadLine();
+            template += "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">\r\n    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>\r\n    <link rel=\"stylesheet\" href=\"../fooldal/css/timeline.css\">\r\n    <link rel=\"icon\" type=\"image/x-icon\" href=\"https://ckik.hu/_next/image?url=https%3A%2F%2Fceg-kozgazdasagi.cms.intezmeny.edir.hu%2Fuploads%2Fthumbnail_ckik_logo1_5ddb5979c8.png&w=128&q=90\">\r\n    <!--link rel=\"stylesheet\" href=\"../../style.css\"-->\r\n    <title>CKIK - " + cim + "</title>\r\n</head>\r\n<body>\r\n    <div class=\"information\">\r\n        <header>\r\n            <h1>"+cim+"</h1>\r\n        </header>\r\n        <main>\r\n            <div class=\"container-fluid\">\r\n                <div class=\"row\">\r\n                    <div class=\"col-12 col-sm-8 szoveg\">";
+            while (!r.EndOfStream)
+            {
+                string line = r.ReadLine();
+                if (line == "")
+                {
+                    string bekcim = r.ReadLine();
+                    if (bekcim == "Tábla:")
+                    {
+                        template += "</div>\r\n                    <!-- d-none d-sm-block -->\r\n                    <div class=\" col-12 col-sm-4 \"> \r\n                        <div class=\"table-wrapper\">\r\n                            <table>\r\n                                <thead>\r\n                                    <tr>\r\n                                        <td colspan=\"2\">\r\n                                          <div id=\"carouselExample\" class=\"carousel slide\">\r\n                                              <div class=\"carousel-inner\">\r\n                                                <div class=\"carousel-item active\">\r\n                                                  <img src=\"../kepek/szamitogep_alex_teszt.png\" class=\"d-block w-100\" alt=\"\" title=\"\">\r\n                                                </div>\r\n                                                <div class=\"carousel-item\">\r\n                                                  <img src=\"../kepek/szamitogep_alex_teszt.png\" class=\"d-block w-100\" alt=\"\" title=\"\">\r\n                                                </div>\r\n                                                <div class=\"carousel-item\">\r\n                                                  <img src=\"../kepek/szamitogep_alex_teszt.png\" class=\"d-block w-100\" alt=\"\" title=\"\">\r\n                                                </div>\r\n                                              </div>\r\n                                              <button class=\"carousel-control-prev\" type=\"button\" data-bs-target=\"#carouselExample\" data-bs-slide=\"prev\">\r\n                                                <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>\r\n                                                <span class=\"visually-hidden\">Previous</span>\r\n                                              </button>\r\n                                              <button class=\"carousel-control-next\" type=\"button\" data-bs-target=\"#carouselExample\" data-bs-slide=\"next\">\r\n                                                <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>\r\n                                                <span class=\"visually-hidden\">Next</span>\r\n                                              </button>\r\n                                            </div>\r\n                                        </td>\r\n                                    </tr>\r\n                                </thead>\r\n                                <tbody>";
+                    }
+                    else
+                    {
+                        template += "                    \r\n<h2>" + bekcim + "</h2>                    \r\n<hr>                    \r\n<p>";
+                    }
+                }
+                else if (line.Contains("|"))
+                {
+                    string[] szet = line.Split('|');
+                    template += "                                    <tr>\r\n                                        <td class=\"tablasorcim\">" + szet[0] + "</td>\r\n                                        <td>" + szet[1] + "</td>\r\n                                    </tr>";
+                }
+                else if (line != "Tábla:")
+                {
+                    template += "\r\n" + line;
+                }
+            }
+            template += "                                </tbody>\r\n                            </table>\r\n                        </div>\r\n                    </div>\r\n                </div>\r\n                <!--<div class=\"row\"> \r\n                    <div class=\"col-12 col-sm-8\"> \r\n                        <h2>Fordította/Készítette:</h2>\r\n                            <hr>\r\n                            <p></p>    \r\n                            <h2>Forrás:</h2>\r\n                            <hr>\r\n                            <a class = \"nemtudtamkitalalnikreativnevet\" href=\"\" target=\"_blank\"></a>\r\n                    </div>\r\n                </div>-->\r\n            </div>\r\n        </main>\r\n        <p style=\"text-align: right;\">\r\n            <button type=\"button\" class=\"btn btn-secondary\" onclick=\"location.href='../fooldal/index.html';\">Vissza</button>\r\n        </p>    \r\n    </div>\t\t\r\n</body>\r\n<script src=\"../tldr.js\"></script>\r\n</html>";
+            r.Close();
+            StreamWriter f = new StreamWriter("gen.html");
+            f.WriteLine(template);
+            f.Close();
+        }
+    }
+}
